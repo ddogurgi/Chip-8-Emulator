e14ce958def6069aa3d39cee45ce0aa4ba129832
ddogurgi <ddogurgi@gmail.com>
2024-06-28 16:28:41 +0900

    chip8.cpp

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Chip-8-Emulator
+
+This is Chip-8-Emulator with C++/SFML by ddogurgi(Wonho Lee)
+
+### References
+1. Guide to making a CHIP-8 emulator -- [Link](https://tobiasvl.github.io/blog/write-a-chip-8-emulator/)
--- chip8.hpp
+++ chip8.hpp
@@ -1,2 +1,27 @@
 #include <iostream>
+#include <array>
+#include <stack>
 #include <SFML/Graphics.hpp>
+
+
+class Chip8 {
+    public:
+        void Initialize();
+        void LoadProgram(const std::string& filename);
+        void EmulateCycle();
+        void DrawGraphics(sf::RenderWindow &window);
+        void SetKeys(sf::Event &event);
+
+    private:
+        std::array<uint8_t, 4096> memory{};//4KB direct-access Memory 
+        //std::array<bool, 64*32> display{}; //64x32 monochrome pixels of display
+        
+        uint16_t I = 0; //16-bit address register
+        uint16_t pc = 0x200; //16-bit program counter(starts at 0x200)
+        std::stack<uint16_t> stack; // A stack which is used to call subroutines/functions and return for them
+        uint8_t delay_timer; //8-bit delay timer, dec at a rate of 60Hz
+        uint8_t sound_timer; //8-bit sound timer
+        
+        std::array<uint8_t, 16> V{}; //16 8-bit general-purpose registers
+        
+};
